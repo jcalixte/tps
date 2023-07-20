390f34012e734355950db976a44aabd41ca32304
Julien Calixte <julienc@bam.tech>
2023-07-21 00:25:17 +0200

    ✨ (vite) feature flow as a VueJS app

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main id="main-app">
+    <ul>
+      <li>
+        <header>Pitch</header>
+        <section class="doing">
+          <h5>📝</h5>
+          <ul>
+            <li>Feature 3</li>
+            <li>Feature 4</li>
+          </ul>
+        </section>
+        <section class="done">
+          <h5>✅</h5>
+          <ul>
+            <li class="blue-bin">feature 1</li>
+            <li class="blue-bin">feature 2</li>
+          </ul>
+        </section>
+      </li>
+      <li>
+        <header>Design</header>
+        <section class="doing">
+          <h5>📝</h5>
+        </section>
+        <section class="done">
+          <h5>✅</h5>
+        </section>
+      </li>
+      <li>
+        <header>Investigation</header>
+        <section class="doing">
+          <h5>📝</h5>
+        </section>
+        <section class="done">
+          <h5>✅</h5>
+        </section>
+      </li>
+      <li>
+        <header>Product design</header>
+        <section class="doing">
+          <h5>📝</h5>
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
+        <section class="done">
+          <h5>✅</h5>
+          <div class="blue-bin">Blue bucket</div>
+          <div class="blue-bin">Blue bucket</div>
+        </section>
+      </li>
+      <li>
+        <header>Development</header>
+        <section class="doing">
+          <h5>📝</h5>
+        </section>
+        <section class="done">
+          <h5>✅</h5>
+        </section>
+      </li>
+      <li>
+        <header>UAT</header>
+        <section class="doing">
+          <h5>📝</h5>
+        </section>
+        <section class="done">
+          <h5>✅</h5>
+        </section>
+      </li>
+      <li>
+        <header>Release</header>
+        <section class="doing">
+          <h5>📝</h5>
+        </section>
+        <section class="done">
+          <h5>✅</h5>
+        </section>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<!-- <style scoped lang="scss">
+#main-app {
+}
+</style> -->
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
 export default {
+  plugins: [vue()],
   build: {
     rollupOptions: {
       input: {
-        index: "index.html",
-        "feature-flow": "feature-flow.html",
-      },
-    },
+        index: 'index.html',
+        'feature-flow': 'feature-flow.html'
+      }
+    }
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'node-fetch': 'isomorphic-fetch'
+    }
+  }
 }
