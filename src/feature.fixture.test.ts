2d9006b4c9ed2e062c79b27bd4235c9f5f04c606
Julien Calixte <julienc@bam.tech>
2023-07-22 11:13:54 +0200

    ✨ (feature) init feature game

---
+++ src/feature.fixture.test.ts
@@ -0,0 +1,10 @@
+import { faker } from '@faker-js/faker'
+import { describe, it } from 'vitest'
+
+describe('feature fixture', () => {
+  it('creates lots of animals', () => {
+    for (let i = 0; i < 200; i++) {
+      console.log(`"${faker.animal.dog()}", `)
+    }
+  })
+})
