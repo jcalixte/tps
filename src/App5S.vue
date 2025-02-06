b21c779723382c47a9946af95c9b49960ec95b45
Julien Calixte <julienc@bam.tech>
2025-02-06 22:48:28 +0100

    Starting the little conversation

--- src/App5S.vue
+++ src/App5S.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main id="main-app">Exploring 5S coming soon</main>
+  <main id="main-app">
+    Exploring 5S coming soon
+    <article>
+      <p>
+        "This my friend, he says calmly, is what makes the whole shop works.
+        Everything I know, everything I need is written down on this piece of
+        paper. Please, take care of it."
+      </p>
+      <p>
+        As you look down at the paper, you see a bunch of list, todos and notes
+        in a brown used paper. It looks like it lived more than you.
+      </p>
+    </article>
+  </main>
 </template>
 
 <style scoped lang="scss">
