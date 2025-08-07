44344fcc872f0b9d8b366dcb4cc435df3135900e
Julien Calixte <julienc@bam.tech>
2025-08-07 09:50:20 +0200

    feat: adding story parts

--- src/App5S.vue
+++ src/App5S.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
 import BoardGameWorkshop from '@/modules/5s/BoardGameWorkshop.vue'
+import { ref } from 'vue'
 
 const createdAt = new Date('2025-01-08').toLocaleDateString(undefined, {
   year: 'numeric',
   month: 'long',
   day: 'numeric'
 })
+
+const storyParts = [
+  'introduction',
+  'first-play',
+  'conclusion-first-play',
+  'seiri',
+  'second-play',
+  'conclusion-second-play',
+  'seiton',
+  // until this point, every improvement is only on chess boards
+  'seiso',
+  'seiketsu', // now we improve everywhere!
+  'shitsuke' // aaaand conclusion
+]
+
+const storyIndex = ref(0)
 </script>
 
 <template>
